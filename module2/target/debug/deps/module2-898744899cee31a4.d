1a927569b0f08eae36c2c219c71b2f2090628628
Rahul <rahul@riodefi.com>
2022-11-11 19:25:52 +0530

    module2_updated

---
+++ module2/target/debug/deps/module2-898744899cee31a4.d
@@ -0,0 +1,5 @@
+/home/sangeet/RustModule-2/module2/target/debug/deps/module2-898744899cee31a4.rmeta: src/main.rs
+
+/home/sangeet/RustModule-2/module2/target/debug/deps/module2-898744899cee31a4.d: src/main.rs
+
+src/main.rs:
---
+++ module2/target/debug/deps/module2-9194721f3dbfc6b4.d
@@ -0,0 +1,5 @@
+/home/sangeet/RustModule-2/module2/target/debug/deps/module2-9194721f3dbfc6b4.rmeta: src/main.rs
+
+/home/sangeet/RustModule-2/module2/target/debug/deps/module2-9194721f3dbfc6b4.d: src/main.rs
+
+src/main.rs:
